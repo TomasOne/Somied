8aec0050b67923cba282d8e7d97d008a2c7d450b
Tomas <onetto_tomas@hotmail.com>
2023-10-29 13:22:20 -0300

    Bucle infinito eliminado. Cards principales funcan

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -14,7 +14,23 @@ export class CardsComponent implements OnInit{
 
   }
 
+  getRace() {
+    this.requestService.getLastRaceHttpClient().subscribe({
+      next: (response: any) => {
+        // Accede a la información que necesitas directamente
+        const raceInfo = response.MRData.RaceTable.Races[0];
+        this.raceResults = raceInfo.Results; // Asigna la información a la propiedad raceInfo
+
+        // Luego, puedes imprimir o utilizar esta información como desees
+        //console.log('Resultados de la carrera:', this.raceInfo);
+      },
+      error: (error) => {
+        console.error('Error al obtener la carrera:', error);
+      }
+    });
+  }
+
   ngOnInit(): void {
-    //this.getRace();
+    this.getRace();
   }
 }
