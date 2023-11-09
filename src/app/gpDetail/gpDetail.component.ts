fe32bc50a18fa56d4a1458c92ddbb3548bee28e5
Walter <wally.moretti@gmail.com>
2023-11-08 21:20:37 -0300

    Se agrega gpDetail + HTML y CSS del mismo

---
+++ src/app/gpDetail/gpDetail.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { RequestService } from '../services/request.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-season',
+  templateUrl: './gpDetail.component.html',
+  styleUrls: ['./gpDetail.component.css']
+})
+export class GpDetailComponent implements OnInit {
+
+  race: any = 0;
+  dataSource: any[] = [];
+  gpDetailData: any[] = [];
+  displayedColumnsGP: string[] = ['position', 'driver', 'constructor', 'laps', 'status', 'points'];
+
+  constructor(private route: ActivatedRoute, private requestService: RequestService) { }
+
+  ngOnInit(): void {
+    this.getRaceID();
+  }
+
+  getRaceID() {
+    this.route.params.subscribe(parameter => {
+      this.race = +parameter['raceID'];
+    });
+
+    this.getGpResult();
+  }
+
+  getGpResult() {
+
+    console.log(this.race);
+    this.requestService.getAllRacesCurrentSeason(this.race).subscribe({
+      next: (response: any) => {
+        const gpData = response.MRData.RaceTable.Races[0].Results;
+        this.gpDetailData = gpData;
+        this.dataSource = this.gpDetailData;
+        console.log(this.dataSource);
+      },
+      error: (error) => {
+        console.error('Error al obtener la informacion de la Carrera: ', error);
+      }
+    });
+  }
+}
