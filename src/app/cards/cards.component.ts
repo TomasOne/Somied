0e02623d6d62cd277817d67b2f3171080623d8db
Walter <wally.moretti@gmail.com>
2023-11-08 21:22:33 -0300

    Navigate to GP-Details y modificacion del HTML

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { forkJoin } from 'rxjs';
 import { RequestService } from 'src/app//services/request.service';
 
@@ -13,8 +14,7 @@ export class CardsComponent implements OnInit {
   //raceResults: any[] = [];
   seasonRaceResults: any[] = [];
 
-  constructor(private requestService: RequestService) { }
-
+  constructor(private router: Router, private requestService: RequestService) { }
 
 
   ngOnInit(): void {
@@ -69,6 +69,13 @@ export class CardsComponent implements OnInit {
       }
     } */
 
+  goToGpResults(raceID: number) {
+
+    if (raceID) {
+      this.router.navigate(['/gp-detail', raceID]);
+    }
+  }
+
   getAllRaces() {
     const observables = [];
 
@@ -88,27 +95,4 @@ export class CardsComponent implements OnInit {
       }
     });
   }
-
-
-  /* Test con retorno de una Promesa en vez de un JSON.
-  getAllRaces() {
-
-    for (let race = 1; race <= 19; race++) {
-
-      let data = this.requestService.getAllRacesCurrentSeason(race);
-      data.then(response => {
-
-        response.subscribe({
-          next: (response: any) => {
-            const raceInfo = response.MRData.RaceTable.Races[0];
-            this.seasonRaceResults.push(raceInfo);
-          },
-          error: (error) => {
-            console.error('Error al obtener la informacion de la Carrera: ', error);
-          }
-        })
-      });
-    }
-  }
-  */
 }
